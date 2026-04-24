0041b1004a9ec1bce80235ef1d9a41f6e93d2b2d
uyen <ng.uyen2703207@gmail.com>
2026-04-24 13:54:45 +0700

    Add code.py file

--- cod1e.py
+++ cod1e.py
@@ -1,1 +1,19 @@
-# ransanmoi
+import pygame
+pygame.init()
+screen= pygame.display.set_mode((600,600))
+pygame.display.set_caption("Snake")
+running=True
+GREEN=(0,200,0)
+clock=pygame.time.Clock()
+
+while running:
+    clock.tick(60)
+    screen.fill(GREEN)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running=False
+    pygame.display.flip()
+
+
+pygame.quit()
