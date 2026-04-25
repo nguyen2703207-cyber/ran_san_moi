4a8ff631514b47d735e0c879f57f1d98645b8809
uyen <ng.uyen2703207@gmail.com>
2026-04-25 16:56:03 +0700

    Add code.py file

--- code.py
+++ code.py
@@ -1,19 +1,22 @@
 import pygame
+from time import sleep
 pygame.init()
 
 
 # Thiết lập giao diện
-screen= pygame.display.set_mode((600,602))  # màn hình 600x602, 602 chừa ra để thấy đường line rõ hơn
+screen= pygame.display.set_mode((601,601))  # màn hình 601x601, 601 chừa ra để thấy đường line rõ hơn
 pygame.display.set_caption("Snake")  # tiêu đề
 running=True
 # các biến màu
-GREEN=(0,200,0)
+GREEN=(0,255,0)
 BLACK=(0,0,0)
 WHITE=(255,255,255)
 clock=pygame.time.Clock()
 
-# vẽ rắn 
-snake= [0,1]
+# tọa độ rắn  
+snakes= [[5,6],[5,7],[5,8]]
+# hướng con rắn ban đầu (dùng lập trình cho con rắn chạy tự động)
+direction= "right"
 while running:
     clock.tick(60)
     screen.fill(BLACK)
@@ -23,10 +26,39 @@ while running:
         pygame.draw.line(screen, WHITE,(0,i*30),(600,i*30))  # vẽ đường thẳng nằm ngang
         pygame.draw.line(screen, WHITE, (30*i,0),(30*i,600))  # vẽ đường thẳng dọc
 
-    pygame.draw.rect(screen, GREEN, ()())
+    # vẽ con rắn
+    for snake in snakes:  # duyệt qua list snakes
+        pygame.draw.rect(screen, GREEN, (snake[0]*30, snake[1]*30, 30, 30))  # vẽ đầu con rắn hình vuông (2 phần đầu là vị trí, 2 phần sau là kích thước)
+
+    # di chuyển rắn tự động
+    if direction == "right":
+        snakes.append([snakes[-1][0]+1, snakes[-1][1]])
+        snakes.pop(0)
+    if direction == "left":
+        snakes.append([snakes[-1][0]-1, snakes[-1][1]])
+        snakes.pop(0)
+    if direction == "up":
+        snakes.append([snakes[-1][0], snakes[-1][1]-1])  # thêm 1 phần tử vào cuối có tọa độ x, y vào list snakes (đi lên thì x giữ nguyên, y giảm 1)
+        snakes.pop(0)
+    if direction == "down":
+        snakes.append([snakes[-1][0], snakes[-1][1]+1])
+        snakes.pop(0)
+
+    sleep(0.05)  # lệnh này để con rắn chạy chậm lại (dừng màn hình, số càng lớn rắn càng chậm)
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running=False
+        if event.type == pygame.KEYDOWN:  # kiểm tra khi nhấn nút trên bàn phím
+            if event.key == pygame.K_UP and direction!= "down":
+                direction = "up"
+            if event.key ==pygame.K_DOWN and direction != "up":
+                direction = "down"
+            if event.key == pygame.K_LEFT and direction != "right":
+                direction = "left"
+            if event.key == pygame.K_RIGHT and direction != "left":
+                pass
+
     pygame.display.flip()
 
 
