fddc065ca68e1b8081ee03932610bad1247c7ed2
nguyen2703207-cyber <ng.uyen2703207@gmail.com>
2026-04-26 12:12:56 +0700

    Delete tempCodeRunnerFile.py

--- tempCodeRunnerFile.py
+++
@@ -1,3 +0,0 @@
-for i in range (0,21):
-    #     pygame.draw.line(screen, WHITE,(0,i*30),(600,i*30))  # vẽ đường thẳng nằm ngang
-    #     pygame.draw.line(screen, WHITE, (30*i,0),(30*i,600))  # vẽ đường thẳng dọc
